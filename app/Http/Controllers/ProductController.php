e1ba4d2276fc36755be29497649be29e74223475
koraygurkan <koraygurkandanaci@gmail.com>
2024-05-31 22:58:47 +0300

    api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Porduct;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Porduct  $porduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Porduct $porduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Porduct  $porduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Porduct $porduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Porduct  $porduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Porduct $porduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Porduct  $porduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Porduct $porduct)
+    {
+        //
+    }
+}
