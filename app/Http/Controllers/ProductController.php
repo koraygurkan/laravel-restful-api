6b07d167011870846d8fd5393d7a31830a08ba13
koraygurkan <koraygurkandanaci@gmail.com>
2024-06-06 22:58:18 +0300

    .

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Porduct;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -41,10 +41,10 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Porduct  $porduct
+     * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function show(Porduct $porduct)
+    public function show(Product $product)
     {
         //
     }
@@ -52,10 +52,10 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Porduct  $porduct
+     * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function edit(Porduct $porduct)
+    public function edit(Product $product)
     {
         //
     }
@@ -64,10 +64,10 @@ class ProductController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Porduct  $porduct
+     * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Porduct $porduct)
+    public function update(Request $request, Product $product)
     {
         //
     }
@@ -75,10 +75,10 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Porduct  $porduct
+     * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Porduct $porduct)
+    public function destroy(Product $product)
     {
         //
     }
