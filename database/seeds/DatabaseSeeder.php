c4ae1037fbdab23e77476c27e6cf7cc8c73f563d
koraygurkan <koraygurkandanaci@gmail.com>
2024-06-08 22:24:38 +0300

    .

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Product;
 use App\User;
+use App\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,14 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(CourseTableSeeder::class);
         //$this->call(CoursemTableSeeder::class);
         //$this->call(UserTableSeeder::class);
-        $this->call(  CategoriesTableSeeder::class);
+       // $this->call(  CategoriesTableSeeder::class);
 
         
         DB::statement("TRUNCATE TABLE products"); //product tablosunun içerisini boşaltır
         DB::statement("TRUNCATE TABLE users");
+        DB::statement("TRUNCATE TABLE categories");
 
         //veri ekleme
         DB::table('users')->insert([
@@ -34,6 +35,17 @@ class DatabaseSeeder extends Seeder
 
         //factory ile rastgele veri ekleme
         factory(Product::class,1000)->create();
-       // factory(User::class,10)->create();
+        factory(Category::class,50)->create();
+        factory(User::class,10)->create();
+
+        //veri ekleme
+        DB::table('product_categories')->insert
+        (
+            [
+            'product_id' => '1',
+            'categories_id' => '1'
+            ]
+        );
+
     }
 }
