b314a012f1663cc2c5e9ed89971e1a41545a30f9
koraygurkan <koraygurkandanaci@gmail.com>
2024-06-11 21:25:07 +0300

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email',128)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name', 50)->nullable();
+            $table->string('last_name',50)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
