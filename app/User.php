47ed66e6b24fad1f410fea3379bca6f13f051319
koraygurkan <koraygurkandanaci@gmail.com>
2024-06-11 21:00:21 +0300

    .

--- app/User.php
+++ app/User.php
@@ -35,5 +35,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        //'created_at'=>'date',
+        'created_at'=>'date:Y-m-d'
     ];
 }
