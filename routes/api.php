dbc1be3c2b0f7a1f40b7727ddd8111647326bce6
koraygurkan <koraygurkandanaci@gmail.com>
2024-07-24 01:11:05 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::get('/users',function (){
 //Route::apiResource('users','Api/UserController');
 //Route::apiResource('categories','Api/CategoryController');
 
-Route::get('categories/custom1','Api\CategoryController@custom1');
+Route::get('categories/custom1','Api\CategoryController@custom1')->middleware('auth:api');
 Route::get('products/custom1','Api\ProductController@custom1');
 Route::get('products/custom2','Api\ProductController@custom2');
 Route::get('categories/report1','Api\CategoryController@report1');
