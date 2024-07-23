37da66fe05c611cb9141b16ab77f44b22f787e5f
koraygurkan <koraygurkandanaci@gmail.com>
2024-07-23 23:52:59 +0300

    .

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+//        Auth::User()->name;
+        $user=Auth::User();
+        //Setlocal ile dili değiştirebiliriz.
+//        \App::setLocale('en');
+
+        $locale=\App::getLocale();
+        $str = strtoupper($locale);
+
+        return view('home', $user)->with('dil',$str);
+
+    }
+}
