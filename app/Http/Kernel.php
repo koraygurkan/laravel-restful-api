3e17393017793c149f04f63705898742c11511fa
koraygurkan <koraygurkandanaci@gmail.com>
2024-07-26 00:14:19 +0300

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'CheckAge' => \App\Http\Middleware\CheckAge::class,
+        'api'=> \App\Http\Middleware\ApiToken::class
     ];
 }
