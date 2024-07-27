5b049201d90c7f8089f852d62414a1bf33e01415
koraygurkan <koraygurkandanaci@gmail.com>
2024-07-27 23:58:17 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Auth Basic 
 Route::middleware('auth.basic')->get('/user-basic', function (Request $request) {
     return $request->user();
 });
