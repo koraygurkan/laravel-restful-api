8510824e793a1e6648ee34b48ef042cb91bb7927
koraygurkan <koraygurkandanaci@gmail.com>
2025-03-09 00:44:38 +0300

    .

---
+++ app/Http/Middleware/ApiLogger.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiLogger
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate(Request $request){
+        
+    }
+}
